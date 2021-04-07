ce7d0e5d5261aef1ba172f20c61731935ec01e0f
Karem Cristine de Souza <karem@clickmulti.com.br>
2021-04-06 22:54:27 -0300

    front do pdf

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <tr>
+         <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+      <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+      
+      </tr>
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+    </table>
+    <button @click="getUrl">Add</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from './services/api'
+
 
 export default {
-  name: 'App',
+
   components: {
-    HelloWorld
+
+  },
+  methods:{
+  async  getUrl(){
+  axios({
+  url: '/generate-pdf',
+  method: 'GET',
+  responseType: 'blob', // important
+}).then((response) => {
+  const url = window.URL.createObjectURL(new Blob([response.data]));
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', 'file.pdf');
+  document.body.appendChild(link);
+  link.click();
+});
+     
+    }
   }
 }
 </script>
@@ -24,5 +59,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
 }
 </style>
